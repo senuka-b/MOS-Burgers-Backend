7da4bfeda8350e98a23335b6d174e7cf1680898d
yetimeh <senukabandara2006@gmail.com>
2025-03-17 16:26:33 +0530

    Change Order Entity name to Orders

--- src/main/java/edu/icet/senuka/entity/OrderEntity.java
+++ src/main/java/edu/icet/senuka/entity/OrderEntity.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "Order")
+@Entity(name = "Orders")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
